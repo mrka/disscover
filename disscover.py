1456f317570bf3dfc44b7516d7068753335a3336
Michael Köllejan <michael.koellejan@gmx.net>
2012-08-26 23:26:43 +0200

    PEP8

--- disscover.py
+++ disscover.py
@@ -9,27 +9,29 @@ from IPy import *
 DNS.ParseResolvConf()
 
 # Request IP for host name given in first argument
-dnsAnswer = DNS.DnsRequest(name = sys.argv[1], qtype='A').req()
+dnsAnswer = DNS.DnsRequest(name=sys.argv[1], qtype='A').req()
 
 # Exit if no DNS A record was found for the host name
 if len(dnsAnswer.answers) == 0:
-	sys.exit()
+    sys.exit()
 
 # Either use the host name as domain name or use the second argument if given
 if len(sys.argv) > 2:
-   domainNameGiven = sys.argv[2]
+    domainNameGiven = sys.argv[2]
 else:
-   domainNameGiven = sys.argv[1]
+    domainNameGiven = sys.argv[1]
 
 firstIPForHostname = IP(dnsAnswer.answers[0]['data'])
 domainNameSearched = domainNameGiven
 
 # Keep iterating as long as we stay inside the given domain
-while (domainNameSearched[-len(domainNameGiven):len(domainNameSearched)] == domainNameGiven):
-   reversedIPArray = string.split(firstIPForHostname.strNormal(),'.')
-   reversedIPArray.reverse()
-   dnsAnswerTwo = DNS.DnsRequest(name = string.join(reversedIPArray,'.')+'.in-addr.arpa', qtype='PTR').req()
-   firstIPForHostname = IP(firstIPForHostname.int() + 1)
-   if len(dnsAnswerTwo.answers) != 0:
-      domainNameSearched = dnsAnswerTwo.answers[0]['data']
-      print domainNameSearched
+while (domainNameSearched[-len(domainNameGiven):len(domainNameSearched)]
+        == domainNameGiven):
+    reversedIPArray = string.split(firstIPForHostname.strNormal(), '.')
+    reversedIPArray.reverse()
+    dnsAnswerTwo = DNS.DnsRequest(name=string.join(reversedIPArray, '.') +
+                                  '.in-addr.arpa', qtype='PTR').req()
+    firstIPForHostname = IP(firstIPForHostname.int() + 1)
+    if len(dnsAnswerTwo.answers) != 0:
+        domainNameSearched = dnsAnswerTwo.answers[0]['data']
+        print domainNameSearched
